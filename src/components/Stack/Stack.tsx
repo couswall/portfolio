908b0f69bcf0eecce7896b79269d6b1a2d1d87ec
Andre Ignorosa <andreiglu@hotmail.com>
2025-09-17 23:44:10 -0600

    feat: add animation to stack name tag in StackIcon

--- src/components/Stack/Stack.tsx
+++ src/components/Stack/Stack.tsx
@@ -1,3 +1,4 @@
+"use client";
 import { FaHtml5 } from "react-icons/fa";
 import { FaCss3 } from "react-icons/fa";
 import { RiTailwindCssFill } from "react-icons/ri";
@@ -17,77 +18,32 @@ import StackIcon from "@components/Stack/StackIcon";
 
 const Stack = () => {
   const skillsArray = [
-    {
-      id: 1,
-      name: "HTML5",
-      icon: <FaHtml5 className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 2,
-      name: "CSS",
-      icon: <FaCss3 className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 3,
-      name: "Tailwind",
-      icon: <RiTailwindCssFill className="dark:text-white text-4xl" />,
-    },
+    { id: 1, name: "HTML5", icon: FaHtml5, hoverColor: "#E34F26" },
+    { id: 2, name: "CSS3", icon: FaCss3, hoverColor: "#1572B6" },
+    { id: 3, name: "Tailwind", icon: RiTailwindCssFill, hoverColor: "#38BDF8" },
     {
       id: 4,
-      name: "Javascript",
-      icon: <IoLogoJavascript className="dark:text-white text-4xl" />,
+      name: "JavaScript",
+      icon: IoLogoJavascript,
+      hoverColor: "#F7DF1E",
     },
-    {
-      id: 5,
-      name: "Typescript",
-      icon: <SiTypescript className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 6,
-      name: "React.js",
-      icon: <FaReact className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 7,
-      name: "Redux",
-      icon: <SiRedux className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 8,
-      name: "Next.js",
-      icon: <RiNextjsFill className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 9,
-      name: "Node.js",
-      icon: <FaNodeJs className="dark:text-white text-4xl" />,
-    },
+    { id: 5, name: "TypeScript", icon: SiTypescript, hoverColor: "#3178C6" },
+    { id: 6, name: "React.js", icon: FaReact, hoverColor: "#61DAFB" },
+    { id: 7, name: "Redux", icon: SiRedux, hoverColor: "#764ABC" },
+    { id: 8, name: "Next.js", icon: RiNextjsFill, hoverColor: "#000000" },
+    { id: 9, name: "Node.js", icon: FaNodeJs, hoverColor: "#339933" },
     {
       id: 10,
       name: "PostgreSQL",
-      icon: <BiLogoPostgresql className="dark:text-white text-4xl" />,
+      icon: BiLogoPostgresql,
+      hoverColor: "#4169E1",
     },
-    {
-      id: 11,
-      name: "Prisma",
-      icon: <SiPrisma className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 12,
-      name: "MongoDB",
-      icon: <DiMongodb className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 13,
-      name: "Git",
-      icon: <FaGitAlt className="dark:text-white text-4xl" />,
-    },
-    {
-      id: 14,
-      name: "Github",
-      icon: <FaGithub className="dark:text-white text-4xl" />,
-    },
+    { id: 11, name: "Prisma", icon: SiPrisma, hoverColor: "#0C344B" },
+    { id: 12, name: "MongoDB", icon: DiMongodb, hoverColor: "#47A248" },
+    { id: 13, name: "Git", icon: FaGitAlt, hoverColor: "#F05032" },
+    { id: 14, name: "GitHub", icon: FaGithub, hoverColor: "#181717" },
   ];
+
   return (
     <section
       id="skills"
@@ -98,7 +54,7 @@ const Stack = () => {
           {"Skills & Tecnologies"}
         </h2>
         <div className="flex flex-col items-center justify-center">
-          <div className="group relative inline-block">
+          <div className="relative inline-block">
             <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-4 lg:grid-cols-6 gap-8">
               {skillsArray.map((skill) => (
                 <StackIcon skill={skill} key={skill.id} />
--- src/components/Stack/StackIcon.tsx
+++ src/components/Stack/StackIcon.tsx
@@ -1,4 +1,5 @@
 "use client";
+import { IconType } from "react-icons";
 import React, { useState } from "react";
 
 export interface IStackIconProps {
@@ -8,7 +9,8 @@ export interface IStackIconProps {
 export interface ISkill {
   id: number;
   name: string;
-  icon: React.ReactNode;
+  icon: IconType;
+  hoverColor: string;
 }
 
 const StackIcon: React.FC<IStackIconProps> = ({ skill }) => {
@@ -16,22 +18,28 @@ const StackIcon: React.FC<IStackIconProps> = ({ skill }) => {
   return (
     <div className="group relative inline-block" key={skill.id}>
       {showNameTag && (
-        <div
-          className="absolute -top-10 left-1/2 z-3 -translate-x-1/2 rounded bg-black px-3 py-1 text-sm text-white shadow-xl"
-          style={{
-            whiteSpace: "nowrap",
-            transform: "translateX(-50%) rotate(-5deg)",
-          }}
-        >
+        <div className="animate-bounce-smooth absolute -top-10 left-5 rotate-6 whitespace-nowrap z-3 rounded bg-black px-3 py-1 text-sm text-white shadow-xl">
           {skill.name}
         </div>
       )}
       <div
-        className="p-4 border-2 rounded-full transition-all duration-500 transform dark:border-white hover:scale-125 hover:-translate-y-1 hover:rotate-45"
+        className={
+          "p-4 border-2 rounded-full transition-all duration-500 transform dark:border-white hover:scale-125 hover:-translate-y-1 hover:rotate-45"
+        }
         onMouseEnter={() => setShowNameTag(true)}
         onMouseLeave={() => setShowNameTag(false)}
+        style={{
+          borderColor: showNameTag ? skill.hoverColor : undefined,
+          backgroundColor: showNameTag
+            ? `${skill.hoverColor}20`
+            : "transparent",
+        }}
       >
-        {skill.icon}
+        <skill.icon
+          className={`
+            text-4xl dark:text-white transition-colors duration-500`}
+          style={{ color: showNameTag ? skill.hoverColor : undefined }}
+        />
       </div>
     </div>
   );
